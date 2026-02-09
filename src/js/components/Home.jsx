346d618d2cf6d281f77acda5fb358b73a66e8c0e
belkisAle <belkismarquezblanco@gmail.com>
2026-02-09 17:42:12 +0000

    para entregar

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,32 +1,25 @@
 import React from "react";
 
-<<<<<<< HEAD
 
 import Cards from "./Cards";
 import Jumbotron from "./Jumbotron";
-=======
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
->>>>>>> 39121a5857720d4ae5b7ccfd7f1e10f2b4b663b7
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-<<<<<<< HEAD
+		<NavBar/>
 		<Jumbotron/>
 		<Cards/>
-		</>
-=======
-		<NavBar/>
 		<Footer/>
 
 		</>
 		
 		
->>>>>>> 39121a5857720d4ae5b7ccfd7f1e10f2b4b663b7
 	);
 };
 
