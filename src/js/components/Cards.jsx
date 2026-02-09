2d2b635c76eb14e9b8c76d38c3befe6586213847
belkisAle <belkismarquezblanco@gmail.com>
2026-02-09 12:58:44 +0000

    cards y jumbotron

---
+++ src/js/components/Cards.jsx
@@ -0,0 +1,59 @@
+function Cards(props) {
+    return (
+        <>
+            <div className="row mx-5">
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="https://placehold.co/500x325" class="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem aliquam eligendi quod soluta rem sunt hic. Sunt, maiores explicabo veniam repellendus laboriosam odit vitae porro fugit alias eaque facilis?</p>
+                            </div>
+                            <div className="card-footer text-center">
+                                <a href="#" className="btn btn-primary">Find Out More!</a>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100">
+                        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur unde, blanditiis vitae impedit illum fugit maiores? Dicta, neque voluptates aspernatur esse placeat quibusdam asperiores iste dolor accusantium, quae aliquam eveniet.</p>
+                            </div>
+                            <div className="card-footer text-center">
+                                <a href="#" className="btn btn-primary">Find Out More!</a>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div class="card h-100">
+                        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At sapiente pariatur culpa ipsum maxime possimus consectetur dolor fuga vel veniam, facere ipsam asperiores esse distinctio, impedit dicta optio assumenda accusamus.</p>
+                            </div>
+                            <div className="card-footer text-center">
+                                <a href="#" className="btn btn-primary ">Find Out More!</a>
+                            </div>
+                    </div>
+                </div>
+                 <div className="col">
+                    <div class="card h-100">
+                        <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum sit maxime et cum voluptates iusto quo neque officiis quam, nam laudantium expedita molestias qui est aliquid eveniet inventore, recusandae laborum.</p>
+                            </div>
+                            <div className="card-footer text-center">
+                                <a href="#" className="btn btn-primary">Find Out More!</a>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Cards
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,16 @@
+function Jumbotron(props) {
+    return (
+        <>
+            <div className="card ">
+                <div className="card-body bg-body-secondary m-5">
+                    <h1 className="card-title mt-5 mx-3">A Warm Welcome</h1>
+                    <p className="card-text mx-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, consequatur quod modi dicta, inventore neque iusto officiis quo sequi eos perferendis debitis. Optio deserunt in laborum inventore sed delectus quo.</p>
+                    <a href="#" className="btn btn-primary mb-5 mx-3">Call to action!!</a>
+                </div>
+            </div>
+        </>
+
+    )
+
+}
+export default Jumbotron
