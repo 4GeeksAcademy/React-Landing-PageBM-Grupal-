dd62c9fd11163f642036c163730f641938650570
patrcarrasco <pat.r.carrasco@gmail.com>
2026-02-09 12:28:24 +0000

    NavBar y Footer

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(props) {
+    return (
+        <footer className="bg-dark text-white text-center">
+            <div className="container">
+                <p> Copyright &copy; Your Website 2026</p> 
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
